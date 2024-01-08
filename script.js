e3eb5e1c6804e1fee436f9e10329884758589570
maymikb <123998301+maymikb@users.noreply.github.com>
2024-01-08 14:18:35 -0800

    weather APIs

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-var weatherApiKey = 286d713499b8262e987f2093acdccf8a
+var textArea= $("#city")
+var button=$("#search")
+var h1 = $("#titleHeader")
+ var temp=$("#temp")
+ var wind= $("#wind")
+ var humidity=$("#humidity")
+
+ button.on("click", function(){
+     var weatherApiKey = "286d713499b8262e987f2093acdccf8a";
+
+     var cityName = textArea.val();
+
+     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + weatherApiKey + "&units=imperial")
+     .then(res => res.json())
+     .then(data => {
+        console.log(data);
+
+        var currentDate = dayjs().format("(MMMM DD, YYYY)")
+
+        h1.text(data.name + " " + currentDate)
+
+        temp.text("Temp: " + data.main.temp)
+
+        wind.text("Wind Speed: " + data.wind.speed)
+
+        humidity.text("Humidity: " + data.main.humidity)
+     })
+
+ })
 
 
-var city;
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+// var city;
+
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// //current weather data API
